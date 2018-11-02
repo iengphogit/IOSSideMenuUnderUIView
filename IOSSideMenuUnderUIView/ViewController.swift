aa83a95c80329da17f0306474c4cff29b848ad3d
iengpho <ieng.pho@gaea-sys.com>
2018-11-02 08:58:58 +0700

    done

--- IOSSideMenuUnderUIView/ViewController.swift
+++ IOSSideMenuUnderUIView/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var trailingConstrain: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstrain: NSLayoutConstraint!
+    var openMenu = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainView.layer.shadowOpacity = 1
+        mainView.layer.shadowRadius = 6
     }
 
-
+    @IBAction func openMenu(_ sender: Any) {
+        if openMenu {
+            leadingConstrain.constant = 0
+            trailingConstrain.constant = 0
+        } else {
+            leadingConstrain.constant = 100
+            trailingConstrain.constant = 100
+        }
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        openMenu = !openMenu
+    }
+    
 }
 
